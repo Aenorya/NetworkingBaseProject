39b41d5a346bd80881667bf33f3690f478ca5833
Aenorya <marie.legrand.dev@gmail.com>
2023-06-04 23:12:16 +0200

    client server double project

---
+++ SERVER/SimpleServer.cpp
@@ -0,0 +1,15 @@
+#include <SDL.h>
+#include <SDL_net.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	if (SDLNet_Init() == -1) {
+		cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
+		return 1;
+	}
+
+	cout << "Thank you for using ChArtFX !\n";
+	return 0;
+}
